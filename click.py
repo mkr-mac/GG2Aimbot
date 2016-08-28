8270d0284bef2b782ec9a38ab0a558e177d1df61
CorruptData <mccargishm@gmail.com>
2016-08-27 21:53:32 -0400

    Click slower

--- click.py
+++ click.py
@@ -1,6 +1,7 @@
-import win32api, win32con
+import win32api, win32con, time
+
 def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-click(240,240)
+	win32api.SetCursorPos((x,y))
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+	time.sleep(0.1)
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
