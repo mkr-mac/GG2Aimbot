5d9dbf45c300b3e87de72c26a22399314a878213
CorruptData <mccargishm@gmail.com>
2016-08-27 20:35:03 -0400

    Scanner for Walls

---
+++ clear_path.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import math
+
+def clear_path(x1, y1, x2, y2, screen):
+	m = -(y2-y1)/(x2-x1)
+	pict = Image.open(screen).convert('RGB')
+	if(math.fabs(m)>1):
+		for y in range(y1, y2):
+			xpix = (m*(y1-y))+x1
+			r, g ,b = pict.getpixel((math.floor(xpix), y))
+			if (r == 0 and g == 0 and b == 0):
+				print("done")
+				return False
+		return True
+	else:
+		for x in range(x1, x2):
+			ypix = (m*(x1-x))+y1
+			r, g ,b = pict.getpixel((x, math.floor(ypix)))
+			if (r == 0 and g == 0 and b == 0):
+				print("done")
+				return False
+		return True
+
+scan(412, 200, 600, 600, "boom_headshot.png")
